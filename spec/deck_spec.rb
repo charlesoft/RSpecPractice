2b9d5388026408edeffa866aa1a31bb4f3ffafd5
charlesoft <charlees1509@hotmail.com>
2016-08-15 19:45:02 -0300

    Adding custom matchers.

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,6 +1,32 @@
 require 'deck'
 
+module ArrayMatchers
+  extend RSpec::Matchers::DSL
+
+  matcher(:be_contiguous_by) do
+    match do |array|
+      # array
+      # .sort
+      # .each_cons(2)
+      # .all? {|x,y| x + 1 == y}
+      !first_non_contiguous_pair(array)
+    end
+
+    failure_message do |array|
+      "%s and %s were not contiguous" % first_non_contiguous_pair(array)
+    end
+
+    def first_non_contiguous_pair(array)
+      array
+      .sort_by(&block_arg)
+      .each_cons(2)
+      .detect {|x,y| block_arg.call(x) + 1 != block_arg.call(y)}
+    end
+  end
+end
+
 describe 'Deck' do
+  include ArrayMatchers
 
   describe '.all' do
     it 'contains 32 cards' do
@@ -17,6 +43,10 @@ describe 'Deck' do
       # end
       # expect(Deck.all.map { |card| card.rank }).to all(be >= 7)
       expect(Deck.all).to all(have_attributes(rank: be >= 7))
+    end
+
+    it 'has contiguous ranks by suit' do
+      expect(Deck.all.group_by {|card| card.suit }.values).to all(be_contiguous_by(&:rank))
     end
   end
 end
